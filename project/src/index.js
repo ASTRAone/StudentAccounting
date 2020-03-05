361c4f163bfe7746bdcd19c9ffffcf47c457e485
Vlad <vladwaylvl827@gmail.com>
2020-03-05 22:22:33 +0300

    Update index.js

--- project/src/index.js
+++ project/src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
-import StudentAddForm from './student-add-form';
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(<StudentAddForm />, document.getElementById('root2'));
