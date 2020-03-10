d2ec7a89a66173dead2041747e0454d4435aed65
RagsTUD2020 <xiii.chaos666@gmail.com>
2020-03-10 09:38:18 +0300

    Fucked ups

--- project/src/students-list-element/students-list-element.js
+++ project/src/students-list-element/students-list-element.js
@@ -16,39 +16,39 @@ export default class StudentsListElement extends Component {
                     <p className = "list-element-date__label">Дата заявки</p>
                     <p className = "list-element-date__value">28.01.2020</p>
                 </div>
-                <img src = { noavatar } className = "list__profile-pic" />
-                <p className = "list__name">Иванов Иван Иванович</p>
-                <div className = "list__info">
-                    <div className = "list__contacts__element">
+                <img src = { noavatar } className = "profile-pic" />
+                <p className = "name">Иванов Иван Иванович</p>
+                <div className = "contacts">
+                    <div className = "contacts__element">
                         <i className="fa fa-envelope"></i>
-                        <p className = "list__contacts__text">ivanov@mail.ru</p>
+                        <p className = "contacts__text">ivanov@mail.ru</p>
                     </div>
-                    <div className = "list__contacts__element">
+                    <div className = "contacts__element">
                         <i className ="fa fa-mobile contact-icon"></i>
-                        <p className = "list__contacts__text">89543892398</p>
+                        <p className = "contacts__text">89543892398</p>
                     </div>
                 </div>
-                <div className = "list__info">
-                    <div className = "list__info-element">
-                        <p className = "list__info-label">Учебное заведение:</p>
-                        <p className = "list__info-text">КГУ</p>
+                <div className = "contacts">
+                    <div className = "info-element">
+                        <p className = "info-label">Учебное заведение:</p>
+                        <p className = "info__text">КГУ</p>
                     </div>
-                    <div className = "list__info-element">
-                        <p className = "list__info-label">Факультет, специальность:</p>
-                        <p className = "list__info-text">ИАСТ</p>
-                    </div>
-                </div>
-                <div className = "list__info">
-                    <div className = "list__info-element">
-                        <p className = "list__info-label">Предполагаемые сроки практики:</p>
-                        <p className = "list__info-text">1.07.2019 - 17.07.2019</p>
-                    </div>
-                    <div className = "list__info-element">
-                        <p className = "list__info-label">Желаемое направление деятельности:</p>
-                        <p className = "list__info-text">Разработка</p>
+                    <div className = "info-element">
+                        <p className = "info-label">Факультет, специальность:</p>
+                        <p className = "info__text">ИАСТ</p>
                     </div>
                 </div>
-                <div className = "list__buttons">
+                <div className = "contacts">
+                    <div className = "info-element">
+                        <p className = "info-label">Предполагаемые сроки практики:</p>
+                        <p className = "info__text">1.07.2019 - 17.07.2019</p>
+                    </div>
+                    <div className = "info-element">
+                        <p className = "info-label">Желаемое направление деятельности:</p>
+                        <p className = "info__text">Разработка</p>
+                    </div>
+                </div>
+                <div className = "buttons">
                     <i className ="fa fa-check-circle"></i>
                     <i className ="fa fa-ban"></i>
                 </div>
