8a104901d500905c76c0e3bc94ecc691f7920745
RagsTUD2020 <xiii.chaos666@gmail.com>
2020-03-16 10:13:20 +0300

    Student-list-elements updated (Roma)

--- project/src/pages/students-list-element/students-list-element.js
+++ project/src/pages/students-list-element/students-list-element.js
@@ -15,7 +15,7 @@ export default class StudentsListElement extends Component {
 
     render(){
 
-        const { id, date, SecondName } = this.props;
+        const { id, date, SecondName, FirstName, Patronymic, Speciality, College, Faculty, PractiesBegining, PractiesEnding, Phone, Email } = this.props;
 
         return(
             <div className="list-element">
@@ -28,35 +28,35 @@ export default class StudentsListElement extends Component {
                     <p className = "list-element-date__value">{ date }</p>
                 </div>
                 <img src = { noavatar } className = "list__profile-pic" />
-                <p className = "list__name">Иванов Иван Иванович</p>
+                <p className = "list__name">{ SecondName + " " + FirstName + " " + Patronymic }</p>
                 <div className = "list__info">
                     <div className = "list__contacts__element">
                         <i className="fa fa-envelope"></i>
-                        <p className = "list__contacts__text">ivanov@mail.ru</p>
+                        <p className = "list__contacts__text">{ Email }</p>
                     </div>
                     <div className = "list__contacts__element">
-                        <i className ="fa fa-mobile contact-icon"></i>
-                        <p className = "list__contacts__text">89543892398</p>
+                        <i className ="fa fa-mobile list__contact-icon"></i>
+                        <p className = "list__contacts__text">{ Phone }</p>
                     </div>
                 </div>
                 <div className = "list__info">
                     <div className = "list__info-element">
                         <p className = "list__info-label">Учебное заведение:</p>
-                        <p className = "list__info-text">КГУ</p>
+                        <p className = "list__info-text">{ College }</p>
                     </div>
                     <div className = "list__info-element">
                         <p className = "list__info-label">Факультет, специальность:</p>
-                        <p className = "list__info-text">ИАСТ</p>
+                        <p className = "list__info-text">{ Faculty }</p>
                     </div>
                 </div>
                 <div className = "list__info">
                     <div className = "list__info-element">
                         <p className = "list__info-label">Предполагаемые сроки практики:</p>
-                        <p className = "list__info-text">1.07.2019 - 17.07.2019</p>
+                        <p className = "list__info-text">{ PractiesBegining + " - " + PractiesEnding }</p>
                     </div>
                     <div className = "list__info-element">
                         <p className = "list__info-label">Желаемое направление деятельности:</p>
-                        <p className = "list__info-text">Разработка</p>
+                        <p className = "list__info-text">{ Speciality }</p>
                     </div>
                 </div>
                 <div className = "list__buttons">
--- project/src/pages/students-list/students-list.js
+++ project/src/pages/students-list/students-list.js
@@ -13,10 +13,10 @@ export default class StudentsList extends Component {
     }
 
     item = [
-        { id: 1, date: '11.02.2019', SecondName: 'Иванов', FirstName: 'Иван', Patronymic: 'Иванович', Speciality: 'ИАСТ', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivan@mail.com' },
-        { id: 2, date: '11.02.2019', SecondName: 'Иванов', FirstName: 'Иван', Patronymic: 'Иванович', Speciality: 'ИАСТ', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivan@mail.com' },
-        { id: 3, date: '11.02.2019', SecondName: 'Иванов', FirstName: 'Иван', Patronymic: 'Иванович', Speciality: 'ИАСТ', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivan@mail.com' },
-        { id: 4, date: '11.02.2019', SecondName: 'Иванов', FirstName: 'Иван', Patronymic: 'Иванович', Speciality: 'ИАСТ', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivan@mail.com' }
+        { id: 1, date: '11.02.2019', SecondName: 'Иванов', FirstName: 'Иван', Patronymic: 'Иванович', College: 'КГУ', Faculty: 'ИАСТ', Speciality: 'Разработчик',  PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivanov@mail.com' },
+        { id: 2, date: '11.02.2019', SecondName: 'Чижов', FirstName: 'Владимир', Patronymic: 'Иванович', College: 'Политех', Faculty: 'Программирование', Speciality: 'Тестировщик', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivanov@mail.com' },
+        { id: 3, date: '11.02.2019', SecondName: 'Скворцов', FirstName: 'Николай', Patronymic: 'Иванович', College: 'КГУ', Faculty: 'ФАСТ', Speciality: 'Разработчик', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivanov@mail.com' },
+        { id: 4, date: '11.02.2019', SecondName: 'Сидоров', FirstName: 'Иван', Patronymic: 'Иванович', College: 'КГУ', Faculty: 'ИАСТ', Speciality: 'Разработчик', PractiesBegining: '12.04.2019', PractiesEnding: '12.09.2019', Phone: '8 (999) 245-45-65', Email: 'ivanov@mail.com' }
     ];
 
     render() {
@@ -27,7 +27,7 @@ export default class StudentsList extends Component {
             const { ...rest } = item;
 
             return (      
-                <li className="list-element" key={ id }>
+                <li className="list-element-block" key={ id }>
                     {/* Этот элемент должен быть не зависимым */}
                     <StudentsListElement { ...rest }/>
                 </li>
@@ -36,10 +36,8 @@ export default class StudentsList extends Component {
 
         return (
             <ul className="list">
-                {/* Здесь должна быть одно переменная items */}
-                <StudentsListElement/>
-                <StudentsListElement/>
-                <StudentsListElement/>
+                { items }
+
             </ul>
         );
     };
